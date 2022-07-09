0076f34155d8610a9cb7e72da2e98aa03fd5ebc3
Kiss Róbert <ert78gb@gmail.com>
2022-07-09 20:56:05 +0200

    test: use top level await

--- test/fastify-not-in-gcp.test.js
+++ test/fastify-not-in-gcp.test.js
@@ -7,16 +7,13 @@ import lifecycles from './_helpers/lifecycles.js'
 delete process.env.K_SERVICE
 
 // dynamically load the library to apply the environment variables before the import happens
-import('../lib/index.js')
-	.then(module => module.default)
-	.then(fastifyServerOptions => {
-		lifecycles(tap)
-		commonTests(tap, fastifyServerOptions)
+const {default: fastifyServerOptions} = await import('../lib/index.js')
+lifecycles(tap)
+commonTests(tap, fastifyServerOptions)
 
-		tap.test('trace id should be a number', async ({equal, match}) => {
-			const {response, log} = await getResponseWithLog(fastifyServerOptions)
+tap.test('trace id should be a number', async ({equal, match}) => {
+	const {response, log} = await getResponseWithLog(fastifyServerOptions)
 
-			equal(response.statusCode, 200)
-			match(log['logging.googleapis.com/trace'], /\d*/)
-		})
-	})
+	equal(response.statusCode, 200)
+	match(log['logging.googleapis.com/trace'], /\d*/)
+})
