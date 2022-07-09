8ca0ed5cc35e22c6aa8d8628328fc3bd79823783
Róbert Kiss <erT78gb@gmail.com>
2022-07-09 21:42:07 +0200

    test: use top level await (#4)

--- test/fastify-in-gcp.test.js
+++ test/fastify-in-gcp.test.js
@@ -7,39 +7,36 @@ import lifecycles, {GCP_PROJECT} from './_helpers/lifecycles.js'
 process.env.K_SERVICE = 'fastify-service'
 
 // dynamically load the library to apply the environment variables before the import happens
-import('../lib/index.js')
-	.then(module => module.default)
-	.then(fastifyServerOptions => {
-		lifecycles(tap)
-		commonTests(tap, fastifyServerOptions)
+const {default: fastifyServerOptions} = await import('../lib/index.js')
+lifecycles(tap)
+commonTests(tap, fastifyServerOptions)
 
-		tap.test(`"logging.googleapis.com/trace" should starts with "projects/${GCP_PROJECT}/traces/"`, async ({
-			equal,
-			match,
-		}) => {
-			const {response, log} = await getResponseWithLog(fastifyServerOptions)
+tap.test(`"logging.googleapis.com/trace" should starts with "projects/${GCP_PROJECT}/traces/"`, async ({
+	equal,
+	match,
+}) => {
+	const {response, log} = await getResponseWithLog(fastifyServerOptions)
 
-			equal(response.statusCode, 200)
-			match(log['logging.googleapis.com/trace'], new RegExp(`^projects\\/${GCP_PROJECT}\\/traces\\/`))
-		})
+	equal(response.statusCode, 200)
+	match(log['logging.googleapis.com/trace'], new RegExp(`^projects\\/${GCP_PROJECT}\\/traces\\/`))
+})
 
-		tap.test('trace id should be an auto generated 32 characters log hexadecimal string if x-cloud-trace-context header not provided', async ({
-			equal,
-			match,
-		}) => {
-			const {response, log} = await getResponseWithLog(fastifyServerOptions)
+tap.test('trace id should be an auto generated 32 characters log hexadecimal string if x-cloud-trace-context header not provided', async ({
+	equal,
+	match,
+}) => {
+	const {response, log} = await getResponseWithLog(fastifyServerOptions)
 
-			equal(response.statusCode, 200)
-			const traceId = log['logging.googleapis.com/trace'].split('/').pop()
-			match(traceId, /^[\dA-Fa-f]{32}$/)
-		})
+	equal(response.statusCode, 200)
+	const traceId = log['logging.googleapis.com/trace'].split('/').pop()
+	match(traceId, /^[\dA-Fa-f]{32}$/)
+})
 
-		tap.test('trace id should be the x-cloud-trace-context header if provided', async ({equal, match}) => {
-			const traceHeader = 'trace-header-value'
-			const {response, log} = await getResponseWithLog(fastifyServerOptions, traceHeader)
+tap.test('trace id should be the x-cloud-trace-context header if provided', async ({equal, match}) => {
+	const traceHeader = 'trace-header-value'
+	const {response, log} = await getResponseWithLog(fastifyServerOptions, traceHeader)
 
-			equal(response.statusCode, 200)
-			const traceId = log['logging.googleapis.com/trace'].split('/').pop()
-			match(traceId, traceHeader)
-		})
-	})
+	equal(response.statusCode, 200)
+	const traceId = log['logging.googleapis.com/trace'].split('/').pop()
+	match(traceId, traceHeader)
+})
